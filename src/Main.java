7e0ba6b1a6ab6a1e23208d1bc9ef5c6df372361e
Your Name <dat054760@gmail.com>
2022-07-26 11:24:21 +0700

    up

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int choice = -1;
+        Scanner input = new Scanner(System.in);
+        while (choice != 0) {
+            System.out.println("Menu");
+            System.out.println("1. Print the rectangle");
+            System.out.println("2. Print the square triangle(The corner is square at 4 different angles: top-left, top-right, botton-left,botton-right)");
+            System.out.println("3. Print isosceles triangle");
+            System.out.println("4. Exit");
+            choice = input.nextInt();
+            switch (choice){
+                case 1:
+                    System.out.println("Print the rectangle");
+                    for (int i = 0; i < 3; i++) {
+                        System.out.println("* * * * * * *");
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i <= 5; i++) {
+                        for (int j = 0; j < i; j++) {
+                            System.out.print("* ");
+                        }
+                        System.out.println("");
+                    }
+                    break;
+                case 3:
+                    for (int i = 7; i >=1  ; i--) {
+                        for (int j = 1; j <=i ; j++) {
+                            System.out.print("*  ");
+                        }
+                        System.out.println("");
+                    }
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("No choice!");
+            }
+        }
+    }
+}
